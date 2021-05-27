8f804fb719b6623f12ddae40a4f09bfc9b0defa9
thakaj <taulant.hakaj@scranton.edu>
2021-05-27 12:56:26 -0400

    commit tree 5587b328a2cb83d3bec11aaa72ed409646104ae0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(collection,name){
+    return collection.filter((driver)=> name.toUpperCase()===driver.toUpperCase())
+}
+
+
+
+
+function fuzzyMatch(collection, name){
+    return collection.filter((driver)=> driver.startsWith(name))
+}
+
+function matchName(collection, name){
+    return collection.filter((driver)=> name===driver.name)
+}
